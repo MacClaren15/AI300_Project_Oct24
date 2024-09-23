e47a7265335d44292f3d470f511906ef3dbe3c4f
Leow_Hoe_Seng <mikeygamedev@gmail.com>
2024-09-23 13:33:37 +0800

    Updated files with new changes

--- src/model.py
+++ src/model.py
@@ -5,4 +5,4 @@ class Model:
         self.model = joblib.load('model/rf_AI300.pkl')
 
     def predict(self, input_features):
-        return self.model.predict_proba(input_features)
+        return self.model.predict(input_features)
